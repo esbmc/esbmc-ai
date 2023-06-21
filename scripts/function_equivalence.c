e79f8d44b3189998abce1d0d37d20f036f63f0a2
Yiannis Charalambous <yiannis128@hotmail.com>
2023-06-21 01:25:06 +0100

    # function_equivalence script
    * Initial commit

---
+++ scripts/function_equivalence.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+// {FUNCTION_OLD}
+
+// {FUNCTION_NEW}
+
+int main()
+{
+    // TODO Need to dynamically build parameters to supply.
+
+    // TODO Need to call old and new functions and compare results.
+
+    // check whether both functions produce the same output
+    // assert(output1 == output2);
+
+    return 0;
+}
