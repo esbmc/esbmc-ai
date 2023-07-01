ce568a979ec992df11c667c783ad3103c6cd2bd0
Yiannis Charalambous <yiannis128@hotmail.com>
2023-07-01 14:13:32 +0100

    Added logging level 2 and 3

--- esbmc_ai_lib/logging.py
+++ esbmc_ai_lib/logging.py
@@ -1,13 +1,29 @@
 # Author: Yiannis Charalambous
 
+"""Logging module for verbose printing."""
+
 verbose: int = 0
 
 
 def set_verbose(level: int) -> None:
+    """Sets the verbosity level."""
     global verbose
     verbose = level
 
 
 def printv(m) -> None:
+    """Level 1 verbose printing."""
     if verbose > 0:
+        print(m)
+
+
+def printvv(m) -> None:
+    """Level 2 verbose printing."""
+    if verbose > 1:
+        print(m)
+
+
+def printvvv(m) -> None:
+    """Level 3 verbose printing."""
+    if verbose > 2:
         print(m)
