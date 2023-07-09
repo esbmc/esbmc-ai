8bfb5362983a537f1f487b0da6958142bc979263
Yiannis Charalambous <yiannis128@hotmail.com>
2023-07-09 02:03:41 +0100

    Updated optimize code command

--- esbmc_ai_lib/commands/optimize_code_command.py
+++ esbmc_ai_lib/commands/optimize_code_command.py
@@ -1,11 +1,15 @@
 # Author: Yiannis Charalambous
 
+import os
 import sys
 from os import get_terminal_size
-
+from typing import Optional
 from typing_extensions import override
+from string import Template
 
-from esbmc_ai_lib.chat_response import json_to_base_message
+from clang.cindex import TranslationUnit
+
+from esbmc_ai_lib.chat_response import json_to_base_messages
 from .chat_command import ChatCommand
 from .. import config
 from ..base_chat_interface import ChatResponse
@@ -16,11 +20,81 @@ from ..logging import printvv
 
 
 class OptimizeCodeCommand(ChatCommand):
+    eq_script_path: str = "scripts/function_equivalence.c"
+
     def __init__(self) -> None:
         super().__init__(
             command_name="optimize-code",
             help_message="Optimizes the code of a specific function or the entire file if a function is not specified. Usage: optimize-code [function_name]",
         )
+
+    def _get_functions_list(
+        self,
+        clang_ast: ast.ClangAST,
+        function_names: list[str],
+    ) -> list[str]:
+        """Returns the functions that are referenced in the specified `clang_ast`.
+
+        If the array is empty, then the entire list of function names in
+        `clang_ast` is returned. If a subset of the functions in `clang_ast` is
+        specified, then that is checked against the functions of `clang_ast` and
+        returned. If there's a function specified that is not inside `clang_ast`
+        then a `ValueError` is raised."""
+        all_function_names: list[str] = [fn.name for fn in clang_ast.get_fn_decl()]
+
+        # If specific function names to optimize have been specified, then
+        # check that they exist.
+        if len(function_names) > 0:
+            for function_name in function_names:
+                if function_name not in all_function_names:
+                    raise ValueError(f"Error: {function_name} is not defined...")
+        else:
+            function_names = all_function_names.copy()
+        return function_names
+
+    def _build_comparison_script(
+        self,
+        old: ast.ClangAST,
+        new: ast.ClangAST,
+        old_decl: FunctionDeclaration,
+        new_decl: FunctionDeclaration,
+    ) -> str:
+        """Builds an ESBMC C source code script that insturcts ESBMC to compare the two functions
+        together. A script template is used."""
+
+        # Load and replace template.
+        with open(self.eq_script_path, "r") as file:
+            template_str: str = file.read()
+        template: Template = Template(template=template_str)
+
+        # TODO Get includes from both files, and include it
+        old_includes: str = "OLD"
+        new_includes: str = "NEW"
+        includes: str = old_includes + "\n" + new_includes
+
+        raise NotImplementedError()
+
+        # Get source code of old and new functions.
+        old_source_code: str = old.get_source_code(declaration=old_decl)
+        new_source_code: str = new.get_source_code(declaration=new_decl)
+
+        # Modify names of underlying cursor before generating source code.
+        old_source_code = old_source_code.replace(
+            old_decl.name, old_decl.name + "_old", 1
+        )
+        new_source_code = new_source_code.replace(
+            new_decl.name, new_decl.name + "_new", 1
+        )
+
+        # TODO build parameter list
+
+        script: str = template.substitute(
+            function_old=old_source_code,
+            function_new=new_source_code,
+            includes=includes,
+        )
+
+        return script
 
     def check_function_equivalence(
         self,
@@ -28,16 +102,8 @@ class OptimizeCodeCommand(ChatCommand):
         new_source_code: str,
         function_name: str,
     ) -> bool:
-        """TODO Implement function equivalence checking:
-        Build source code.
-        1. Include the functions as function_orig and function_new
-        2. In the main file of the function include a declaration variable of
-        nondet_X where X is the type of each parameter that the function
-        accepts.
-        3. Call each function and equate results.
-        """
+        """Function equivalence checking"""
 
-        # Get list of function types.
         original_ast: ast.ClangAST = ast.ClangAST(
             file_path="old.c",
             source_code=original_source_code,
@@ -48,14 +114,54 @@ class OptimizeCodeCommand(ChatCommand):
             source_code=new_source_code,
         )
 
+        # Get list of function types.
         orig_functions: set[FunctionDeclaration] = set(original_ast.get_fn_decl())
         new_functions: set[FunctionDeclaration] = set(new_ast.get_fn_decl())
 
-        if orig_functions != new_functions:
+        # Need to ensure that the functions have the same composition:
+
+        # First get original function declaration from source code.
+        old_function: Optional[FunctionDeclaration] = None
+        for fn in orig_functions:
+            if fn.name == function_name:
+                old_function = fn
+                break
+
+        # Check that the new functions set also has this function. The equivalence of
+        # function declarations will ensure that args are also checked.
+        if old_function is None or old_function not in new_functions:
             return False
 
-        print("WARNING: MOCK FUNCTION DOES NOT VERIFY")
-        print("TODO...")
+        # Get new function
+        new_function: Optional[FunctionDeclaration] = None
+        for fn in new_functions:
+            # The old function and new function are identical, except they have different
+            # underlying cursors.
+            if fn == old_function:
+                new_function = fn
+                break
+
+        if new_function is None:
+            # TODO handle properly
+            raise Exception()
+
+        esbmc_script: str = self._build_comparison_script(
+            old=original_ast,
+            new=new_ast,
+            old_decl=old_function,
+            new_decl=new_function,
+        )
+
+        # TODO Run script with ESBMC.
+        save_path: str = os.path.join(
+            config.temp_file_dir, os.path.basename(self.eq_script_path)
+        )
+        with open(save_path, "w") as file:
+            file.write(esbmc_script)
+
+        # TODO Remove me
+        print(esbmc_script)
+        exit(0)
 
         return True
 
@@ -68,26 +174,19 @@ class OptimizeCodeCommand(ChatCommand):
             source_code=source_code,
         )
 
-        all_functions: list[FunctionDeclaration] = clang_ast.get_fn_decl()
-        all_function_names: list[str] = [fn.name for fn in all_functions]
+        try:
+            function_names = self._get_functions_list(clang_ast, function_names)
+        except ValueError as e:
+            print(e)
+            sys.exit(1)
 
-        # If specific function names to optimize have been specified, then
-        # check that they exist.
-        if len(function_names) > 0:
-            for function_name in function_names:
-                if function_name not in all_function_names:
-                    print(f"Error: {function_name} is not defined...")
-                    sys.exit(1)
-        else:
-            function_names = all_function_names.copy()
+        print(f"Optimizing the following functions: ", ", ".join(function_names), "\n")
 
-        print(f"Optimizing the following functions: {function_names}\n")
-
+        # Declare code optimizer chat.
         chat: OptimizeCode = OptimizeCode(
-            system_messages=[
-                json_to_base_message(msg)
-                for msg in config.chat_prompt_optimize_code.system_messages
-            ],
+            system_messages=json_to_base_messages(
+                config.chat_prompt_optimize_code.system_messages
+            ),
             initial_message=config.chat_prompt_optimize_code.initial_prompt,
             ai_model=config.ai_model,
             llm=config.ai_model.create_llm(
@@ -96,27 +195,30 @@ class OptimizeCodeCommand(ChatCommand):
             ),
         )
 
+        # Loop through every function and generate an improvement.
         new_source_code: str = source_code
         max_retries: int = 10
-        for function in function_names:
+        for fn_name in function_names:
             for attempt in range(max_retries):
-                print(f"Optimizing function: {function}")
+                print(f"Optimizing function: {fn_name}")
                 # Optimize the function
                 response: ChatResponse = chat.optimize_function(
                     source_code=new_source_code,
-                    function_name=function,
+                    function_name=fn_name,
                 )
 
-                printvv(f"\nGeneration ({function}):")
+                printvv(f"\nGeneration ({fn_name}):")
                 printvv("-" * get_terminal_size().columns)
                 printvv(response.message.content)
                 printvv("-" * get_terminal_size().columns)
 
                 # Check equivalence
+                # TODO Get response.message.content code extracted
+                # using the method of solution generation.
                 equal: bool = self.check_function_equivalence(
                     original_source_code=source_code,
-                    new_source_code=new_source_code,
-                    function_name=function,
+                    new_source_code=response.message.content,
+                    function_name=fn_name,
                 )
 
                 # TODO Handle cases where all attempts are failed.
