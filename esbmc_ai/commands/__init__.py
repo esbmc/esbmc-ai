7e1525835609cc5c15687d7e97ef9847e6db2000
Yiannis Charalambous <yiannis128@hotmail.com>
2024-02-03 15:17:34 +0000

    Removed more verify code command refs

--- esbmc_ai/commands/__init__.py
+++ esbmc_ai/commands/__init__.py
@@ -2,12 +2,10 @@ from .chat_command import ChatCommand
 from .exit_command import ExitCommand
 from .fix_code_command import FixCodeCommand
 from .help_command import HelpCommand
-from .verify_code_command import VerifyCodeCommand
 
 __all__ = [
     "ChatCommand",
     "ExitCommand",
     "FixCodeCommand",
     "HelpCommand",
-    "VerifyCodeCommand",
 ]
