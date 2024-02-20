9f3296e2b7fa1899becab7abfc38fd2f8bbe2292
Yiannis Charalambous <yiannis128@hotmail.com>
2024-02-20 20:12:09 +0000

    Implemented the JSON Config Loader

--- esbmc_ai_config/models/json_config_loader.py
+++ esbmc_ai_config/models/json_config_loader.py
@@ -2,25 +2,25 @@
 
 import json
 from dataclasses import dataclass
-from typing import Any
 from typing_extensions import override
 
 from esbmc_ai_config.models.config_loader import ConfigLoader
 
 __ALLOWED_JSON_TYPES = bool | float | int | str | list | dict
 
+# TODO Write tests
+
 
 @dataclass
 class JsonConfigNode:
     default_value: "__ALLOWED_JSON_TYPES" = ""
-    value: "__ALLOWED_JSON_TYPES" = ""
-    """The value in the config. If type does not match the `default_value` type and the
-    `create_missing_fields` is set to `True`, then the `default_value` will be used."""
     is_optional: bool = False
     """Will not load the config if the value is not specified by the user. If
     true will assign default_value."""
     show_in_config: bool = True
     """Show this field in the config tool UI."""
+    _name: str = ""
+    """Initialized automatically"""
 
     def __getitem__(self, subscript):
         """Allow indexing the default_values if they are a list or a dict.
@@ -79,20 +79,49 @@ class JsonConfigLoader(ConfigLoader):
         ), f"{self.file_path} is not a valid json file."
 
         self.root_node: JsonConfigNode = root_node
-        self.values: dict = {}
+        assert isinstance(self.root_node.default_value, dict)
+
+        # TODO Initialize node default fields.
+        self._init_json_nodes(self.root_node)
 
         super().__init__(
             file_path=file_path,
             create_missing_fields=create_missing_fields,
         )
 
+    def _init_json_nodes(self, node: JsonConfigNode) -> None:
+        """Initializes `_name` field of `JsonConfigNode`."""
+        if isinstance(node.default_value, dict):
+            for key, child_node in node.default_value.items():
+                child_node._name = key
+                self._init_json_nodes(child_node)
+
     @override
     def save(self) -> None:
-        raise NotImplementedError()
+        with open(self.file_path, "w") as f:
+            json.dump(self.json_content, f)
+
+    def _init_node_from_default(self, node: JsonConfigNode) -> "__ALLOWED_JSON_TYPES":
+        """Recursively builds a json struct and returns it from the JsonConfigNode"""
+        if isinstance(node.default_value, dict):
+            values: dict = {}
+            for key, value in node.default_value.items():
+                values[key] = self._init_node_from_default(value)
+            return values
+        elif isinstance(node.default_value, list):
+            # FIXME Not supported currently
+            return node.default_value
+        else:
+            return node.default_value
 
     @override
     def _create_default_file(self) -> None:
-        raise NotImplementedError()
+        default_json: "__ALLOWED_JSON_TYPES" = self._init_node_from_default(
+            self.root_node
+        )
+        assert isinstance(default_json, dict)
+        with open(self.file_path, "w") as f:
+            json.dump(default_json, f)
 
     @override
     def _read_fields(self, create_missing_fields: bool = False) -> None:
@@ -104,25 +133,72 @@ class JsonConfigLoader(ConfigLoader):
             is encountered, will instead initialize it to the default value."""
         self.json_content: dict = json.loads(self.content)
 
-        def init_node_value(node: JsonConfigNode, json_node: "__ALLOWED_JSON_TYPES"):
-            # Check if the value and default_value match type
-            if type(json_node) is not type(node.default_value):
-                # Initialize with default_value
-                node.value = node.default_value
-            elif type(node.default_value) is dict[str, JsonConfigNode]:
-                assert isinstance(json_node, dict)
-                # Init children
-                for child_key, child_node in node.default_value:
-                    assert isinstance(child_key, str)
-                    assert isinstance(child_node, JsonConfigNode)
-                    init_node_value(child_node, json_node[child_key])
+        def init_node_value(
+            node: JsonConfigNode, json_node: "__ALLOWED_JSON_TYPES"
+        ) -> "__ALLOWED_JSON_TYPES":
+            """Initializes recursively the json nodes based on the template described in
+            JsonConfigNode."""
+            # Check if types match, if not then initialize to proper type. After this statement
+            # all remaining types should be equal.
+            if type(node.default_value) is not type(json_node):
+                if create_missing_fields or node.is_optional:
+                    return self._init_node_from_default(node)
+                else:
+                    raise ValueError(
+                        f"JsonConfigLoader Error: {node._name} is not of type: "
+                        f"{type(node.default_value)}, instead is {type(json_node)}"
+                    )
             elif isinstance(node.default_value, list):
-                assert isinstance(json_node, list)
-                node.value = json_node
+                # FIXME Lists not supported so they are not touched.
+                return json_node
+            # Recursive case: Check if type is object
+            elif isinstance(node.default_value, dict):
+                assert isinstance(json_node, dict)
+                for name, child in node.default_value.items():
+                    # Check each element of object.
+                    # If node does not exist then create.
+                    if name in json_node:
+                        json_node[name] = init_node_value(child, json_node[name])
+                    elif create_missing_fields or node.is_optional:
+                        json_node[name] = self._init_node_from_default(child)
+                    else:
+                        raise ValueError(
+                            f"JsonConfigLoader Error: {node._name} is not of type: "
+                            f"{type(node.default_value)}, instead is {type(json_node)}"
+                        )
+                return json_node
             else:
-                assert type(json_node) is type(node.default_value)
-                node.value = json_node
+                # Is primitive type so just return
+                return json_node
 
-        init_node_value(self.root_node, self.json_content)
+        # Start with root object.
+        json_content: "__ALLOWED_JSON_TYPES" = init_node_value(
+            self.root_node, self.json_content
+        )
+        assert isinstance(json_content, dict)
 
-        raise Exception(self.root_node.value)
+        self.json_content = json_content
+
+    def get_value(self, *path: str | int) -> "__ALLOWED_JSON_TYPES":
+        current_value: "__ALLOWED_JSON_TYPES" = self.json_content
+        for element in path:
+            if isinstance(current_value, dict):
+                current_value = current_value[element]
+            elif isinstance(current_value, list) and isinstance(element, int):
+                current_value = current_value[element]
+            else:
+                raise IndexError(f"Invalid access {element} from {path}")
+        return current_value
+
+    def set_value(self, value: "__ALLOWED_JSON_TYPES", *path: str | int) -> None:
+        parent_path: tuple[int | str, ...] = path[:-1]
+        parent_element = self.get_value(*parent_path)
+        if isinstance(parent_element, list) and isinstance(path[-1], int):
+            parent_element[path[-1]] = value
+        elif isinstance(parent_element, dict):
+            parent_element[path[-1]] = value
+        else:
+            raise IndexError(
+                f"Invalid set value {value} of type {type(value)} "
+                f"for {path[-1]} from {path}"
+            )
