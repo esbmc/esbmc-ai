e77b748b19ac2ebb8a024e4627b2b3304a550118
Yiannis Charalambous <yiannis128@hotmail.com>
2024-03-28 14:15:15 +0000

    esbmc_util: Added esbmc methods for getting information from the output

--- esbmc_ai/esbmc_util.py
+++ esbmc_ai/esbmc_util.py
@@ -9,6 +9,64 @@ from typing import Optional
 from . import config
 
 
+def esbmc_get_violated_property(esbmc_output: str) -> Optional[str]:
+    """Gets the violated property line of the ESBMC output."""
+    # Find "Violated property:" string in ESBMC output
+    lines: list[str] = esbmc_output.splitlines()
+    for ix, line in enumerate(lines):
+        if "Violated property:" == line:
+            return "\n".join(lines[ix : ix + 3])
+    return None
+
+
+def esbmc_get_counter_example(esbmc_output: str) -> Optional[str]:
+    """Gets ESBMC output after and including [Counterexample]"""
+    idx: int = esbmc_output.find("[Counterexample]\n")
+    if idx == -1:
+        return None
+    else:
+        return esbmc_output[idx:]
+
+
+def esbmc_get_error_type(esbmc_output: str) -> str:
+    """Gets the error of violated property, the entire line."""
+    # TODO Test me
+    # Start search from the marker.
+    marker: str = "Violated property:\n"
+    violated_property_index: int = esbmc_output.rfind(marker) + len(marker)
+    from_loc_error_msg: str = esbmc_output[violated_property_index:]
+    # Find second new line which contains the location of the violated
+    # property and that should point to the line with the type of error.
+    # In this case, the type of error is the "scenario".
+    scenario_index: int = from_loc_error_msg.find("\n")
+    scenario: str = from_loc_error_msg[scenario_index + 1 :]
+    scenario_end_l_index: int = scenario.find("\n")
+    scenario = scenario[:scenario_end_l_index].strip()
+    return scenario
+
+
+def get_source_code_err_line(esbmc_output: str) -> Optional[int]:
+    # Find "Violated property:" string in ESBMC output
+    violated_property: Optional[str] = esbmc_get_violated_property(esbmc_output)
+    if violated_property:
+        # Get the line of the violated property.
+        pos_line: str = violated_property.splitlines()[1]
+        pos_line_split: list[str] = pos_line.split(" ")
+        for ix, word in enumerate(pos_line_split):
+            if word == "line":
+                # Get the line number
+                return int(pos_line_split[ix + 1])
+    return None
+
+
+def get_source_code_err_line_idx(esbmc_output: str) -> Optional[int]:
+    line: Optional[int] = get_source_code_err_line(esbmc_output)
+    if line:
+        return line - 1
+    else:
+        return None
+
+
 def esbmc(path: str, esbmc_params: list, timeout: Optional[float] = None):
     """Exit code will be 0 if verification successful, 1 if verification
     failed. And any other number for compilation error/general errors."""
