0cf2a70884aeddfc93a26907b727506bce326d24
Yiannis Charalambous <yiannis128@hotmail.com>
2024-04-03 13:41:52 +0100

    Updated logging
    Logging now has protected _verbosity value with accessors. Also added method for drawing horizontal lines

--- esbmc_ai/logging.py
+++ esbmc_ai/logging.py
@@ -2,28 +2,42 @@
 
 """Logging module for verbose printing."""
 
-verbose: int = 0
+from os import get_terminal_size
+
+_verbose: int = 0
+
+
+def get_verbose_level() -> int:
+    return _verbose
 
 
 def set_verbose(level: int) -> None:
     """Sets the verbosity level."""
-    global verbose
-    verbose = level
+    global _verbose
+    _verbose = level
 
 
 def printv(m) -> None:
     """Level 1 verbose printing."""
-    if verbose > 0:
+    if _verbose > 0:
         print(m)
 
 
 def printvv(m) -> None:
     """Level 2 verbose printing."""
-    if verbose > 1:
+    if _verbose > 1:
         print(m)
 
 
 def printvvv(m) -> None:
     """Level 3 verbose printing."""
-    if verbose > 2:
+    if _verbose > 2:
         print(m)
+
+
+def print_horizontal_line(verbosity: int) -> None:
+    if verbosity >= _verbose:
+        try:
+            printvv("-" * get_terminal_size().columns)
+        except OSError:
+            pass
