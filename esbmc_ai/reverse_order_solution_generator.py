92086a83ff57e9aa1503689e29f47a4b6f787ecd
Yiannis Charalambous <yiannis128@hotmail.com>
2024-04-19 17:52:39 +0100

    SolutionGenerator: Updated Reverse order to override send message

--- esbmc_ai/reverse_order_solution_generator.py
+++ esbmc_ai/reverse_order_solution_generator.py
@@ -19,53 +19,16 @@ class ReverseOrderSolutionGenerator(SolutionGenerator):
     reverse order."""
 
     @override
-    def generate_solution(self) -> tuple[str, FinishReason]:
-        self.push_to_message_stack(
-            HumanMessage(content=self.ai_model_agent.initial_prompt)
-        )
-
-        # Format source code
-        source_code_formatted: str = get_source_code_formatted(
-            source_code_format=self.source_code_format,
-            source_code=self.source_code_raw,
-            esbmc_output=self.esbmc_output,
-        )
-
-        # Apply template substitution to message stack
-        self.apply_template_value(
-            source_code=source_code_formatted,
-            esbmc_output=self.esbmc_output,
-        )
-
+    def send_message(self, message: Optional[str] = None) -> ChatResponse:
         # Reverse the messages
         messages: list[BaseMessage] = self.messages.copy()
         self.messages.reverse()
 
-        # Generate the solution
-        response: ChatResponse = self.send_message()
+        response: ChatResponse = super().send_message(message)
 
         # Add to the reversed message the new message received by the LLM.
         messages.append(self.messages[-1])
         # Restore
         self.messages = messages
 
-        solution: str = str(response.message.content)
-
-        solution = SolutionGenerator.get_code_from_solution(solution)
-
-        # If source code passed to LLM is formatted then we need to recombine to
-        # full source code before giving to ESBMC
-        match self.source_code_format:
-            case "single":
-                # Get source code error line from esbmc output
-                line: Optional[int] = get_source_code_err_line_idx(self.esbmc_output)
-                if not line:
-                    # Check if it parses
-                    line = get_clang_err_line_index(self.esbmc_output)
-
-                assert (
-                    line
-                ), "fix code command: error line could not be found to apply brutal patch replacement"
-                solution = apply_line_patch(self.source_code_raw, solution, line, line)
-
-        return solution, response.finish_reason
+        return response
---
+++ tests/test_reverse_order_solution_generator.py
@@ -0,0 +1,92 @@
+# Author: Yiannis Charalambous
+
+import pytest
+
+from langchain.schema import HumanMessage, AIMessage, SystemMessage
+from langchain_community.llms.fake import FakeListLLM
+
+from esbmc_ai.ai_models import AIModel
+from esbmc_ai.config import AIAgentConversation, ChatPromptSettings
+from esbmc_ai.reverse_order_solution_generator import ReverseOrderSolutionGenerator
+
+
+@pytest.fixture(scope="function")
+def setup_llm_model():
+    llm = FakeListLLM(
+        responses=[
+            "This is a test response",
+            "Another test response",
+            "One more!",
+        ],
+    )
+    model = AIModel("test model", 1000)
+    return llm, model
+
+
+def test_call_update_state_first(setup_llm_model) -> None:
+    llm, model = setup_llm_model
+
+    chat_settings = ChatPromptSettings(
+        system_messages=AIAgentConversation(
+            messages=(
+                SystemMessage(content="Test message 1"),
+                HumanMessage(content="Test message 2"),
+                AIMessage(content="Test message 3"),
+            ),
+        ),
+        initial_prompt="Initial test message",
+        temperature=1.0,
+    )
+
+    solution_generator = ReverseOrderSolutionGenerator(
+        llm=llm,
+        ai_model=model,
+        ai_model_agent=chat_settings,
+    )
+
+    with pytest.raises(AssertionError):
+        solution_generator.generate_solution()
+
+
+def test_message_stack(setup_llm_model) -> None:
+    llm, model = setup_llm_model
+
+    chat_settings = ChatPromptSettings(
+        system_messages=AIAgentConversation(
+            messages=(
+                SystemMessage(content="Test message 1"),
+                HumanMessage(content="Test message 2"),
+                AIMessage(content="Test message 3"),
+            ),
+        ),
+        initial_prompt="Initial test message",
+        temperature=1.0,
+    )
+
+    solution_generator = ReverseOrderSolutionGenerator(
+        llm=llm,
+        ai_model=model,
+        ai_model_agent=chat_settings,
+    )
+
+    with pytest.raises(AssertionError):
+        solution_generator.generate_solution()
+
+    solution_generator.update_state("", "")
+
+    solution, _ = solution_generator.generate_solution()
+    assert solution == llm.responses[0]
+    solution_generator.ai_model_agent.initial_prompt = "Test message 2"
+    solution, _ = solution_generator.generate_solution()
+    assert solution == llm.responses[1]
+    solution_generator.ai_model_agent.initial_prompt = "Test message 3"
+    solution, _ = solution_generator.generate_solution()
+    assert solution == llm.responses[2]
+
+    # Test history is intact
+    assert solution_generator.messages[0].content == "Initial test message"
+    assert solution_generator.messages[1].content == "This is a test response"
+    assert solution_generator.messages[2].content == "Test message 2"
+    assert solution_generator.messages[3].content == "Another test response"
+    assert solution_generator.messages[4].content == "Test message 3"
+    assert solution_generator.messages[5].content == "One more!"
