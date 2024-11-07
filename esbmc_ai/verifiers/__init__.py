2d3cff75616eea03f5951ca74b43f71821aadd55
Yiannis Charalambous <yiannis128@hotmail.com>
2024-11-07 13:03:55 +0000

    Add BaseSourceVerifier

---
+++ esbmc_ai/verifiers/__init__.py
@@ -0,0 +1,3 @@
+from .base_source_verifier import BaseSourceVerifier
+
+__all__ = ["BaseSourceVerifier"]
---
+++ esbmc_ai/verifiers/base_source_verifier.py
@@ -0,0 +1,9 @@
+# Author: Yiannis Charalambous
+
+"""This module holds the code for the base source code verifier."""
+
+from abc import ABC
+
+
+class BaseSourceVerifier(ABC):
+    """The base class for creating a source verifier for ESBMC-AI."""
