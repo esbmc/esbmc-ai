6a1f587b88838b1903f68918b39d699f80d40cb5
Yiannis Charalambous <yiannis128@hotmail.com>
2025-03-22 18:27:28 +0000

    feat: Added logging functions that contain a prefix message.

--- esbmc_ai/logging.py
+++ esbmc_ai/logging.py
@@ -5,6 +5,7 @@
 from os import get_terminal_size
 
 _verbose: int = 0
+_default_label: str = "ESBMC-AI"
 
 
 def get_verbose_level() -> int:
@@ -17,25 +18,69 @@ def set_verbose(level: int) -> None:
     _verbose = level
 
 
+def set_default_label(label: str) -> None:
+    global _default_label
+    _default_label = label
+
+
+def print_verbose(
+    *m: object,
+    level: int = 0,
+    label: str | None = None,
+    post_label: str | None = None,
+) -> None:
+    """Prints at a verbose level."""
+    if _verbose >= level:
+        if label is None:
+            label = _default_label
+
+        if post_label:
+            label = label + " " + post_label
+
+        # If we have a label in the end, then add the double colon.
+        if label:
+            label += ": "
+
+        print(label, *m)
+
+
+def log0(*m: object, label: str | None = None, post_label: str | None = None) -> None:
+    """Prints at verbose level 0."""
+    print_verbose(level=0, label=label, post_label=post_label, *m)
+
+
+def logv(*m: object, label: str | None = None, post_label: str | None = None) -> None:
+    """Level 1 verbose printing."""
+    print_verbose(level=1, label=label, post_label=post_label, *m)
+
+
+def logvv(*m: object, label: str | None = None, post_label: str | None = None) -> None:
+    """Level 2 verbose printing."""
+    print_verbose(level=2, label=label, post_label=post_label, *m)
+
+
+def logvvv(*m: object, label: str | None = None, post_label: str | None = None) -> None:
+    """Level 3 verbose printing."""
+    print_verbose(level=3, label=label, post_label=post_label, *m)
+
+
 def printv(*m: object) -> None:
     """Level 1 verbose printing."""
-    if _verbose > 0:
-        print(*m)
+    print_verbose(level=1, label="", post_label="", *m)
 
 
 def printvv(*m: object) -> None:
     """Level 2 verbose printing."""
-    if _verbose > 1:
-        print(*m)
+    print_verbose(level=2, label="", post_label="", *m)
 
 
 def printvvv(*m: object) -> None:
     """Level 3 verbose printing."""
-    if _verbose > 2:
-        print(*m)
+    print_verbose(level=3, label="", post_label="", *m)
 
 
-def print_horizontal_line(verbosity: int) -> None:
+def print_horizontal_line(verbosity: int = 0) -> None:
+    """Prints a horizontal line if at a given verbosity level."""
     if _verbose >= verbosity:
         try:
             print("-" * get_terminal_size().columns)
