d0e4708beb9391a5a9625a4592862c0a418beab2
Yiannis Charalambous <yiannis128@hotmail.com>
2025-03-23 00:19:51 +0000

    feat: Added verifier caching of previous verifications

--- esbmc_ai/verifiers/base_source_verifier.py
+++ esbmc_ai/verifiers/base_source_verifier.py
@@ -2,10 +2,17 @@
 
 """This module holds the code for the base source code verifier."""
 
+from pathlib import Path
 import re
 from typing import Any
+from hashlib import sha512
+import pickle
 
+from platformdirs import user_cache_dir
+
+from esbmc_ai.__about__ import __version__ as esbmc_ai_version
 from esbmc_ai.base_component import BaseComponent
+from esbmc_ai.logging import log0, logv
 from esbmc_ai.solution import Solution
 from esbmc_ai.base_config import BaseConfig, default_scenario
 from esbmc_ai.verifier_output import VerifierOutput
@@ -50,14 +57,56 @@ class BaseSourceVerifier(BaseComponent):
         """Alias for name"""
         return self.name
 
+    def _cache_name_pack(self, properties: Any) -> Any:
+        """Packs additional version properties to the cache name in order to ensure
+        it only functions in the current version of ESBMC-AI."""
+        return [esbmc_ai_version, properties]
+
+    def _save_cached(self, properties: Any, result: Any) -> None:
+        """Saves the verification results to a cached directory to be loaded
+        later. Properties are going to be hashed to form the name of the file,
+        they should be anything that defines the file."""
+        properties = self._cache_name_pack(properties)
+        data_dump: bytes = pickle.dumps(obj=properties, protocol=-1)
+        file_id: str = str(sha512(data_dump).hexdigest())
+        log0("Saving result to cache", post_label="Verifier")
+        logv(f"Cache ID: {file_id}", post_label="Verifier")
+
+        cache: Path = Path(user_cache_dir("esbmc-ai", "Yiannis Charalambous"))
+        cache.mkdir(parents=True, exist_ok=True)
+        with open(cache / file_id, "wb") as file:
+            pickle.dump(obj=result, file=file, protocol=-1)
+
+    def _load_cached(self, properties: Any) -> Any:
+        """Loads the verification results from a cached directory."""
+        properties = self._cache_name_pack(properties)
+        data_dump: bytes = pickle.dumps(obj=properties, protocol=-1)
+        file_id: str = str(sha512(data_dump).hexdigest())
+        logv(f"Searching cache ID: {file_id}", post_label="Verifier")
+
+        cache: Path = Path(user_cache_dir("esbmc-ai", "Yiannis Charalambous"))
+        filename: Path = cache / file_id
+        if cache.exists() and filename.exists() and filename.is_file():
+            with open(filename, "rb") as file:
+                data: bytes = pickle.load(file=file)
+                log0("Using cached result", post_label="Verifier")
+                return data
+
+        logv("Cache not found...", post_label="Verifier")
+        return None
+
     def verify_source(
         self,
         solution: Solution,
+        entry_function: str = "main",
+        timeout: int | None = None,
         **kwargs: Any,
     ) -> VerifierOutput:
         """Verifies source_file, the kwargs are optional arguments that are
         implementation dependent."""
         _ = solution
+        _ = entry_function
+        _ = timeout
         _ = kwargs
         raise NotImplementedError()
 
@@ -89,8 +138,22 @@ class BaseSourceVerifier(BaseComponent):
         _ = verifier_output
         return default_scenario
 
-    def get_trace(self, solution: Solution, verifier_output: str) -> list[ProgramTrace]:
-        """Returns the trace given a counterexample."""
+    def get_stack_trace(self, verifier_output: str) -> str:
+        """Gets the stack trace that points to the error."""
+        _ = verifier_output
+        raise NotImplementedError()
+
+    def get_trace(
+        self,
+        solution: Solution,
+        verifier_output: str,
+        include_libs=False,
+        add_missing_source=False,
+    ) -> list[ProgramTrace]:
+        """Returns a more detailed trace. Each line that causes the error is
+        returned. Given a counterexample."""
         _ = solution
         _ = verifier_output
+        _ = include_libs
+        _ = add_missing_source
         raise NotImplementedError()
